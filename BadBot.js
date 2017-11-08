6af5f9ab2607ddbcd400020d7fe66441602fa662
JamesBacon <JamesBacon@users.noreply.github.com>
2017-11-08 13:55:10 +1030

    Made the code neater?

--- BadBot.js
+++ BadBot.js
@@ -9,15 +9,14 @@ client.on('ready', ready => {
 client.on('message', message => {
   if(message.content.startsWith("move")) {
     var getMentions = message.mentions.members.array();
-    var messageContent = message.content;
-    var parts = messageContent.split(" ");
-    var messageLast = parts.slice(-1)[0];
-
+    var messageContent = message.content.split(" ").slice(-1)[0];
+    //var messageLast = parts.slice(-1)[0];
+    
     getMentions.forEach(function(item, index, array) {
-        var channel = message.guild.channels.find('name', messageLast);
+        var channel = message.guild.channels.find('name', messageContent);
         message.guild.member(item).setVoiceChannel(channel);
     });
   }
 });
 
-client.login('Place your bots token here');
+client.login('Your Bot Tocken here');
